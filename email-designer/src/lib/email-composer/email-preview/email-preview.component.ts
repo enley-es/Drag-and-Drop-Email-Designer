567987ee485fcf488ad65966d6dce7cc18fe85ac
barru-fat <abarbero@enley.com>
2025-07-28 15:38:01 +0200

    Add sanitize content to preview screen

--- email-designer/src/lib/email-composer/email-preview/email-preview.component.ts
+++ email-designer/src/lib/email-composer/email-preview/email-preview.component.ts
@@ -1,54 +1,60 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { ConstantsData } from '../constants';
+import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { DomSanitizer } from "@angular/platform-browser";
+import { ConstantsData } from "../constants";
 
 @Component({
-  selector: 'app-email-preview',
-  templateUrl: './email-preview.component.html',
-  styleUrls: ['./email-preview.component.scss'],
+  selector: "app-email-preview",
+  templateUrl: "./email-preview.component.html",
+  styleUrls: ["./email-preview.component.scss"],
 })
 export class EmailPreviewComponent {
   @Output() backToEditor: EventEmitter<any> = new EventEmitter<any>();
   @Input() isPlainText: any;
   @Input() previewContent: any;
-  @Input() previewType: string = 'desktop';
+  @Input() previewType: string = "desktop";
 
   blob!: Blob;
-  blobUrl = '';
+  blobUrl = "";
   bgColor = ConstantsData.bgPickerValue;
+  sanitizedPreviewContent: any;
 
-  constructor() { }
+  constructor(private sanitized: DomSanitizer) {}
 
   ngOnInit() {
     this.blob = new Blob([this.previewContent], { type: "text/html" });
     this.blobUrl = URL.createObjectURL(this.blob);
+
+    this.sanitizedPreviewContent = this.sanitized.bypassSecurityTrustHtml(
+      this.previewContent
+    );
   }
-  
+
   back() {
-    this.backToEditor.emit()
+    this.backToEditor.emit();
   }
-  
+
   onLoad() {}
-  
+
   showDesktop() {
-    this.previewType = 'desktop';
+    this.previewType = "desktop";
   }
-  
+
   showMobile() {
-    this.previewType = 'mobile';
+    this.previewType = "mobile";
   }
 
   showHtml() {
-    this.previewType = 'html';
+    this.previewType = "html";
   }
 
   copyHtml() {
-    let text_elem = document.createElement('textarea');
-    text_elem.style.position = 'absolute';
-    text_elem.style.left = '-9999px';
-    text_elem.style.top = '0';
+    let text_elem = document.createElement("textarea");
+    text_elem.style.position = "absolute";
+    text_elem.style.left = "-9999px";
+    text_elem.style.top = "0";
     text_elem.value = this.previewContent;
     document.body.appendChild(text_elem);
     text_elem.select();
-    document.execCommand('copy');
+    document.execCommand("copy");
   }
 }
